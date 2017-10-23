beb8c4d04ee2dd901cf1038594097f93fd0a58ac
Truong Bao <baotch1312@gmail.com>
2017-10-23 15:08:27 +0700

    show list giang vien in /home public

--- src/controller/PublicIndexController.java
+++ src/controller/PublicIndexController.java
@@ -1,12 +1,16 @@
 package controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import model.bean.User;
+import model.dao.UserDAO;
 
 public class PublicIndexController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -17,6 +21,18 @@ public class PublicIndexController extends HttpServlet {
     
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		//lay danh sach giang vien
+		//noi bang user + hocvi
+		
+		UserDAO userDAO = new UserDAO();
+		
+		ArrayList<User> listUser = new ArrayList<User>();
+		
+		if(listUser != null){
+			  listUser = userDAO.getListGiangVien();
+		}
+		
+		request.setAttribute("listUser", listUser);
 		
 		 RequestDispatcher rd = request.getRequestDispatcher("/index.jsp");
          rd.forward(request, response);
--- src/model/dao/UserDAO.java
+++ src/model/dao/UserDAO.java
@@ -26,7 +26,44 @@ public class UserDAO {
 		  connectMySQLLibrary = new ConnectMySQLLibrary();
 	}
 	
+	//lây danh sach giang vien  23/10/2017
 	
+	public ArrayList<User> getListGiangVien(){
+		ArrayList<User> listUser = new ArrayList<>();
+		conn = connectMySQLLibrary.getConnectMySQL();
+		
+		 String sql = "select u.*, k.tenKhoa, ltk.tenLoaiTaiKhoan, hv.tenHocVi from user AS u "
+	        		+ " INNER JOIN loaitaikhoan AS ltk ON ltk.idLoaiTaiKhoan = u.idLoaiTaiKhoan  "
+	        		+ " INNER JOIN  khoa AS k ON k.idKhoa = u.idKhoa "
+	        		+ " INNER JOIN  hocvi AS hv ON hv.idHocVi = u.idHocVi ";
+		
+	    User objUser = null;
+		  
+		try {
+			st = conn.createStatement();
+			rs = st.executeQuery(sql);
+			
+			while(rs.next()){
+				 objUser = new User(rs.getInt("idUser"),rs.getString("fullName"),rs.getString("chucDanhKhoaHoc") ,rs.getString("diaChiCoQuan") ,
+			             rs.getString("dienThoaiCoQuan"),rs.getInt("idHocVi") ,rs.getString("tenHocVi"),rs.getString("namSinh") ,rs.getString("diaChiNhaRieng") , 
+			             rs.getString("dienThoaiNhaRieng") ,rs.getString("email") ,rs.getString("fax"),rs.getString("userName") , 
+			             rs.getString("matKhau") ,rs.getInt("idLoaiTaiKhoan"),rs.getString("tenLoaiTaiKhoan") ,rs.getInt("idKhoa"), rs.getString("tenKhoa") );
+			                        
+				 listUser.add(objUser);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally{
+			try {
+				st.close();
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return listUser;
+		
+	}
 
 	//Hien thi danh sach thanh vien có phân trang ứng vs id chủ nhiệm đề tài (thèn đang login)
 	
