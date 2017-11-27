f615d1eccd57107b7d371b3116f63172aec22dc7
Truong Bao <baotch1312@gmail.com>
2017-11-27 21:08:58 +0700

    download thuyet minh

--- src/controller/PublicDownloadThuyetMinhController.java
+++ src/controller/PublicDownloadThuyetMinhController.java
@@ -1,6 +1,8 @@
 package controller;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
@@ -15,7 +17,6 @@ import library.LibraryAuth;
 public class PublicDownloadThuyetMinhController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-	private int idDeTai;
 	
     public PublicDownloadThuyetMinhController() {
         super();
@@ -31,6 +32,29 @@ public class PublicDownloadThuyetMinhController extends HttpServlet {
 			return;
 		}
 		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		String filename = "demo11.txt";
+		String filepath = "D:\\";
+		
+		response.setContentType("APPLICATION/OCTET-STREAM");
+		response.setHeader("Content-Disposition", "attachment; filename=\""+ filename + "\"");
+ 
+		 /*use inline if you want to view the content in browser, helpful for
+		 pdf file
+		 response.setHeader("Content-Disposition","inline; filename=\"" +
+		 filename + "\"");*/
+		
+		FileInputStream fileInputStream = new FileInputStream(filepath
+				+ filename);
+ 
+		int i;
+		while ((i = fileInputStream.read()) != -1) {
+			out.write(i);
+		}
+		fileInputStream.close();
+		out.close();
 		
 		
 	}
@@ -38,11 +62,6 @@ public class PublicDownloadThuyetMinhController extends HttpServlet {
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
        
-			  request.setCharacterEncoding("UTF-8");
-			  response.setCharacterEncoding("UTF-8");
-			  response.setContentType("text/html");
-				
-				
 	}
 	
 	
